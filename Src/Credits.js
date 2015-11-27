efdd775255e6fb5305a3a5c1a789c461d7077eda
InsaneAboutTNT <johnathanminecraftbro@gmail.com>
2015-11-28 08:11:37 +1300

    Add note in credits about Javascript/HTML5

--- Src/Credits.js
+++ Src/Credits.js
@@ -1,7 +1,7 @@
 CubeGame.Credits = function() {};
 CubeGame.Credits.prototype = {
     create: function() {
-        this.credits = "Avoid the obstacles with the Cube!\nCreated with the Phaser Game Engine\n\nImages created in Inkscape\nSounds created in Audacity";
+        this.credits = "Avoid the obstacles with the Cube!\nA Javascript/HTML5 Game\nCreated with the Phaser Game Engine\n\nImages created in Inkscape\nSounds created in Audacity";
         this.creditsText = CubeGame.factory.addText(100, 50, this.credits, 30, "#5f5f5f");
         
         this.menuButton = this.game.add.button(100, 400, "Textures", this.menuState, this);
